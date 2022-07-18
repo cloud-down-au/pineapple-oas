954b538db6d2d4e6356e73cec76e01344587b3e2
Kieran Simpson <kieran@clouddown.com.au>
2022-07-18 13:24:13 +1000

    Try to get swagger-ui to work.

--- run-swagger-ui.sh
+++ run-swagger-ui.sh
@@ -2,4 +2,8 @@
 
 PORT=8080
 
-docker run --rm -p 80:$PORT swaggerapi/swagger-ui
+docker run --rm \
+  -p $PORT:8080 \
+  -v $PWD/dist/pineapple-api.yml:/dist/pineapple-api.yml \
+  -e SWAGGER_JSON=/dist/pineapple-api.yml \
+  swaggerapi/swagger-ui:v4.12.0
